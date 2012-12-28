deb4db896752f49400be50931f6c74489310fd33
Matteo Depalo <matteodepalo@gmail.com>
2012-12-28 11:50:05 +0100

    Switched to postgresql database

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,11 +2,13 @@
  
 # --- !Ups
 
+CREATE SEQUENCE round_id_seq;
 CREATE TABLE round (
-  id integer NOT NULL auto_increment PRIMARY KEY,
+  id integer NOT NULL PRIMARY KEY DEFAULT nextval('round_id_seq'),
   game varchar(255)
 );
  
 # --- !Downs
- 
+
+DROP SEQUENCE round_id_seq;
 DROP TABLE round;
--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,8 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "mysql" % "mysql-connector-java" % "5.1.18" 
+    "mysql" % "mysql-connector-java" % "5.1.18",
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
   )
 
   def customLessEntryPoints(base: File): PathFinder = (
