feb6034ef53a6c72e23618615d9b772bdeffe1c2
Matteo Depalo <matteodepalo@gmail.com>
2012-12-31 15:18:12 +0100

    Added game new and create actions
    Removed some comments and useless files

---
+++ app/controllers/Controller.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.api.mvc._
+import play.api.Play
+
+trait Controller extends play.api.mvc.Controller {
+  val adminPassword = Play.current.configuration.getString("adminPassword") match {
+    case Some(x) => x
+    case None => "1234secret" 
+  }
+
+  def Secured[A](username: String, password: String)(action: Action[A]) = Action(action.parser) { request =>
+    request.headers.get("Authorization").flatMap { authorization =>
+      authorization.split(" ").drop(1).headOption.filter { encoded =>
+        new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
+          case u :: p :: Nil if u == username && password == p => true
+          case _ => false
+        }
+      }.map(_ => action(request))
+    }.getOrElse {
+      Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured"""")
+    }
+  }
+}
---
+++ app/controllers/Games.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models.Game
+
+object Games extends Controller {
+  val gameForm = Form(
+    "name" -> nonEmptyText
+  )
+
+  def build = Secured("admin", adminPassword) {
+    Action {
+      Ok(views.html.games.build(gameForm))
+    }
+  }
+
+  def create = Secured("admin", adminPassword) {
+    Action { implicit request =>
+      gameForm.bindFromRequest.fold(
+        errors => BadRequest(views.html.games.build(errors)),
+        name => {
+          Game.create(name)
+          Redirect(routes.Rounds.index)
+        }
+      )
+    }
+  }
+}
