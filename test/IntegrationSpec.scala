c6d9cb73914a291a5101aeed51b0ed82dc4c28c2
Matteo Depalo <matteodepalo@gmail.com>
2013-01-02 21:19:17 +0100

    Put back basic tests

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -5,9 +5,13 @@ import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
 class IntegrationSpec extends Specification {
+  "Application" should {
+    "work from within a browser" in {
+      running(TestServer(3333), HTMLUNIT) { browser =>
+        browser.goTo("http://localhost:3333/")
+        browser.pageSource must contain("Find matches for the games you love around you!")
+      }
+    }
+  }
 }
