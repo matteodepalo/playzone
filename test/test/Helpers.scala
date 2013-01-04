b09ae003af3d1652ffa4ed60b3a803a03a9f0eba
Matteo Depalo <matteodepalo@gmail.com>
2013-01-04 15:44:37 +0100

    Removed superclass from test helpers

--- test/test/Helpers.scala
+++ test/test/Helpers.scala
@@ -6,7 +6,7 @@ import play.api.Play.current
 import play.api.test._
 import play.api.test.Helpers._
 
-object Helpers extends org.specs2.mutable.Specification {
+object Helpers {
   def testDB[T](test: => T) = {
     running(FakeApplication()) {
       DB.withConnection {
