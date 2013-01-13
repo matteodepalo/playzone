14e1e18ec56b5473d5313bd52dcfbb155e19ae84
Matteo Depalo <matteodepalo@gmail.com>
2013-01-13 13:10:11 +0100

    Added a layout to user show action

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,11 +1,10 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 import models.User
 
 object Users extends Controller {
-  def show(id: Long) = Action {
+  def show(id: Long) = Action { implicit request =>
     User.find(id).map { user =>
       Ok(views.html.users.show(user))
     }.getOrElse(NotFound)
